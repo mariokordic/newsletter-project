c0228b08efc88e52a6897541b744dff50def97e9
mariokordic <mario.kordic4@gmail.com>
2017-10-28 12:34:20 +0200

    Update connect_to_mysql.php

--- connect_to_mysql.php
+++ connect_to_mysql.php
@@ -1,10 +1,10 @@
 <?php
-$username = "mbit_wp1";
-$pass = "?*ilpKhdhD^%";
-$dsn = 'mysql:host=localhost;dbname=mbit_biovitamini';
+$username = "";
+$pass = "";
+$dns = 'mysql:host=localhost;dbname='';
 
 try{
-    $conn = new PDO($dsn,$username,$pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn = new PDO($dns,$username,$pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 }catch(PDOException $e)
